6c78a94010cc96fd0b73a0183ce4a89e0b2aaf98
MD G MOHIDDIN <MD G MOHIDDIN@LAPTOP-R4KD4R46>
2021-10-14 13:24:04 +0530

    My second Commit

---
+++ DemoProject/src/test/java/practice/Test2.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		float f=3.5f;
+		char ch= 'A';
+		System.out.println("f="+f);
+		System.out.println("ch="+ch);
+		System.out.println("Run faster and crack interview....");
+	
+    }
+
+}
