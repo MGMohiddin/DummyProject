2b69a85d4ccf91961cf5998dcd768641a6ed9a1f
MD G MOHIDDIN <MD G MOHIDDIN@LAPTOP-R4KD4R46>
2021-10-14 12:56:22 +0530

    My First Commit

---
+++ DemoProject/src/test/java/practice/Test1.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		
+		System.out.println("a="+a);
+	
+		System.out.println("Hi i am here 1st user");
+		
+	}
+
+}
